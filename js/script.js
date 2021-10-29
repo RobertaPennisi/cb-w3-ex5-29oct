9bbd7ffd16aad74b48af921d31599aa801e3d983
Roberta Pennisi <roberta.pennisi97@gmail.com>
2021-10-29 19:43:43 +0200

    adesso è possibile inserire più elementi alla lista

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const titleH1 = document.querySelector("h1");
 const buttonChangeName = document.querySelector(".button-name");
 
 const ourList = document.querySelector(".list");
-const childListElement = document.createElement("li");
 const buttonAddtoList = document.querySelector(".button-list");
 
 buttonChangeName.addEventListener("click", () => {
@@ -15,6 +14,13 @@ buttonChangeName.addEventListener("click", () => {
 
 buttonAddtoList.addEventListener("click", () => {
     const listValue = prompt('Insert list element');
-    ourList.appendChild(childListElement);
-    childListElement.textContent = listValue;
-})
+    addToList(listValue);
+});
+
+const addToList = (event) => {
+    const childListElement = document.createElement("li");
+    const addValue = document.createTextNode(event);
+    childListElement.append(addValue);
+    ourList.append(childListElement);
+
+}
