90ce6bf1abd01568c06fc93e6011cfb264b0fad6
Roberta Pennisi <roberta.pennisi97@gmail.com>
2021-10-29 18:03:34 +0200

    esercizio grezzo ma funzionante

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const firstWrapper = document.querySelector(".first-task");
+const secondWrapper = document.querySelector(".second-task");
+const titleH1 = document.querySelector("h1");
+const buttonChangeName = document.querySelector(".button-name");
+const ourList = document.querySelector(".list");
+const childListElement = document.createElement("li");
+const buttonAddtoList = document.querySelector(".button-list");
+
+buttonChangeName.addEventListener("click", () => {
+    const newTitle = prompt('Insert new title');
+    titleH1.textContent = newTitle;
+});
+
+buttonAddtoList.addEventListener("click", () => {
+    ourList.appendChild(childListElement);
+})
